53b11c7d9dcfe8d6240a1015b65aa778003f8df2
wotrex <steepcraftt@gmail.com>
2020-02-16 14:17:51 +0200

    update 1

--- TelegramBot.py
+++ TelegramBot.py
@@ -32,6 +32,10 @@ class BogdanBot():
         chat_id = update['message']['chat']['id']
         if update['message']['from']['id'] == 395942614 and (update['message']['chat']['type'] == "group" or update['message']['chat']['type'] == "supergroup") :
             return chat_id
+    def get_chat_bog(self,update):  
+        chat_id = update['message']['chat']['id']
+        if update['message']['from']['username'] == 'zagin177' and (update['message']['chat']['type'] == "group" or update['message']['chat']['type'] == "supergroup") :
+            return chat_id
         
     def send_mess(self, chat, text):  
         params = {'chat_id': chat, 'text': text}
@@ -55,7 +59,14 @@ def start():
         if last_update is None:
             continue
         last_update_id = last_update['update_id']
-        r = random.randint(1,14)
+        r = random.randint(1,18)
+        rb = random.randint(1,3)
+        if rb == 1:
+            bot.send_mess(bot.get_chat_bog(bot.last_update()),"+")
+        if rb == 2:
+            bot.send_mess(bot.get_chat_bog(bot.last_update()),"Согласен")
+        if rb == 3:
+            bot.send_mess(bot.get_chat_bog(bot.last_update()),"Поддержую")
         if r == 1:
             bot.send_mess(bot.get_chat_sp(bot.last_update()),"Шпецюк поїш гамна")
         if r == 2:
@@ -85,10 +96,28 @@ def start():
             bot.send_voice(bot.get_chat_sp(bot.last_update()),"https://upload.wikimedia.org/wikipedia/commons/2/2e/Voice3.ogg")
         if r == 14:
             bot.send_voice(bot.get_chat_sp(bot.last_update()),"https://upload.wikimedia.org/wikipedia/commons/b/b0/Voice4.ogg")
+        if r == 15:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"@shputya, ти овощ")
+        if r == 16:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Шпитя ти тупий")
+        if r == 17:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Ти дебіл шпитя")
+        if r == 18:
+            bot.send_voice(bot.get_chat_sp(bot.last_update()),"https://upload.wikimedia.org/wikipedia/commons/b/bf/Voice5.ogg")
+        if r == 19:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Завали їбало")
+        if r == 20:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Пашол нахуй Шпитюк")
+        if r == 21:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Ти підарастіческа хуйня їбана")
+        if r == 22:
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Ти кріпак засраний")
+            bot.send_mess(bot.get_chat_sp(bot.last_update()),"Іди сіно кидай")
+
         rand = random.randint(1,2000)
         if rand == 100:
             bot.send_mess(bot.get_chat_id(bot.last_update()),"Ми не підтримуємо булінг")
-        if bot.get_message(bot.last_update()) == "Богдан, завали єбало":
+        if bot.get_message(bot.last_update()) == "Богдан, завали єбало" or bot.get_message(bot.last_update()) == "Соси" or bot.get_message(bot.last_update()) == "Закрий єбало" or bot.get_message(bot.last_update()) == "Завали єбало" or bot.get_message(bot.last_update()) == "Богдан,закрий єбало" or bot.get_message(bot.last_update()) == "єбало офф" or bot.get_message(bot.last_update()) == "єбало off " or bot.get_message(bot.last_update()) == "Пішов нахуй Богдан" or bot.get_message(bot.last_update()) == "Пішов нахуй" or  bot.get_message(bot.last_update()) == "Пашол нахуй Богдан" or bot.get_message(bot.last_update()) == "Пашол нахуй" or bot.get_message(bot.last_update()) == "Бодя іди нах" or bot.get_message(bot.last_update()) == "іди нах" or bot.get_message(bot.last_update()) == "Бодя іди нахуй" or bot.get_message(bot.last_update()) == "Іди нахуй" or bot.get_message(bot.last_update()) == "Богдан іди нахуй" or bot.get_message(bot.last_update()) == "Богдан пішов нахуй" or bot.get_message(bot.last_update()) == "Богдан пашол нахуй" or bot.get_message(bot.last_update()) == "Shut up" or bot.get_message(bot.last_update()) == "Сосни" or bot.get_message(bot.last_update()) == "Пососи" or bot.get_message(bot.last_update()) == "Пососи ок?" or bot.get_message(bot.last_update()) == "Пососеш ок?" :
             bot.send_mess(bot.get_chat_id(bot.last_update()),"Ок")
             offset = None
             break
@@ -101,7 +130,7 @@ while 1:
     if last_update is None:
             continue
     last_update_id = last_update['update_id']
-    if bot.get_message(bot.last_update()) == "Бодька":
+    if bot.get_message(bot.last_update()) == "Бодька" or bot.get_message(bot.last_update()) == "Богдан" or bot.get_message(bot.last_update()) == "Бодя" or bot.get_message(bot.last_update()) == "Бодька ти тут?" or bot.get_message(bot.last_update()) == "Богдан ти тут?" or bot.get_message(bot.last_update()) == "Бодя ти тут?" or (bot.last_update()) == "Бодька проснись" or bot.get_message(bot.last_update()) == "Богдан проснись" or bot.get_message(bot.last_update()) == "Бодя проснись":
         bot.send_mess(bot.get_chat_id(bot.last_update()),"Де Шпецюк блять")
         start()
     offset = last_update_id + 1
