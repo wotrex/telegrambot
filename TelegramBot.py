f21d62022b934db008d731706abb033c1c07b06a
wotrex <47450238+wotrex@users.noreply.github.com>
2020-05-30 23:50:26 +0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -146,7 +146,7 @@ def getLastMatch(idchat, last_match, matchStats):
             break
     firstBloodPlayer = None
     for n in range(10):
-        if matchStats['players'][n]['kills_log'] is None:
+        if matchStats['players'][n]['kills_log'] is None or not matchStats['players'][n]['kills_log']:
             break
         if int(firstBloodTime) + 1 == int(matchStats['players'][n]['kills_log'][0]['time']):
             if (matchStats['players'][n]['name']) is None:
